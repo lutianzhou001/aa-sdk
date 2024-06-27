0b2a9d98b97492fc3999119124a0231e04ddc014
matthew.yu <matthew.yu@okg.com>
2024-06-27 16:43:25 +0800

    chore: uncomment

--- test/okxSmartAccount/okxSmartAccount.test.ts
+++ test/okxSmartAccount/okxSmartAccount.test.ts
@@ -1,31 +1,31 @@
-// import { sepolia } from "viem/chains";
+import { sepolia } from "viem/chains";
 import { describe } from "node:test";
-// import { Chain, createPublicClient, http, PublicClient } from "viem";
-// import { OKXSmartContractAccount } from "../../packages/okxSmartAccount/OKXSmartContractAccount";
-// import { OKXAASigner } from "../../packages/plugins/interfaces/OKXAASigner";
-// import { LocalAccountAASigner } from "../../packages/plugins/signers/localAccountAASigner";
+import { Chain, createPublicClient, http, PublicClient } from "viem";
+import { OKXSmartContractAccount } from "../../packages/okxSmartAccount/OKXSmartContractAccount";
+import { OKXAASigner } from "../../packages/plugins/interfaces/OKXAASigner";
+import { LocalAccountAASigner } from "../../packages/plugins/signers/localAccountAASigner";
 
 describe("Account Simple Tests", async () => {
-  // const dummyMnemonic = "test test test test test test test test test test test test";
-  // const signer: OKXAASigner = LocalAccountAASigner.mnemonicToAccountSigner(dummyMnemonic);
-  // const chain = sepolia;
-  // const publicClient = createPublicClient({
-  //   chain,
-  //   transport: http(),
-  // });
-  // const givenConnectedProvider = async ({ signer, chain }: { signer: OKXAASigner; chain: Chain }) => {
-  //   const publicClient: PublicClient = createPublicClient({
-  //     chain: chain,
-  //     transport: http(),
-  //   });
-  //   await OKXSmartContractAccount.create({
-  //     rpcProvider: publicClient,
-  //     signer: signer,
-  //     version: "3.0.2",
-  //     index: 0n,
-  //     bundlerClientConfig: {
-  //       bundlerUrl: "https://beta.okex.org",
-  //     },
-  //   });
-  // };
+  const dummyMnemonic = "test test test test test test test test test test test test";
+  const signer: OKXAASigner = LocalAccountAASigner.mnemonicToAccountSigner(dummyMnemonic);
+  const chain = sepolia;
+  const publicClient = createPublicClient({
+    chain,
+    transport: http(),
+  });
+  const givenConnectedProvider = async ({ signer, chain }: { signer: OKXAASigner; chain: Chain }) => {
+    const publicClient: PublicClient = createPublicClient({
+      chain: chain,
+      transport: http(),
+    });
+    await OKXSmartContractAccount.create({
+      rpcProvider: publicClient,
+      signer: signer,
+      version: "3.0.2",
+      index: 0n,
+      bundlerClientConfig: {
+        bundlerUrl: "https://beta.okex.org",
+      },
+    });
+  };
 });
