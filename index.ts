65310bbbbaaf096d89e862d2108ef98396f514aa
vincent.lu <vincent.lu@okg.com>
2024-07-02 16:23:25 +0800

    export types

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export * from "./packages/okxSmartAccount/OKXSmartContractAccount";
 export * from "./packages/okxBundler/bundler";
 export * from "./packages/okxPaymaster/paymaster";
+export * from "./packages/okxSmartAccount/types";
 export * from "./abis";
