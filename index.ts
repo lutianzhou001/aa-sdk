c89a8ac1904b68ed6abc59e5b021669f6484f65a
vincent.lu <vincent.lu@okg.com>
2024-07-10 15:29:47 +0800

    export utils to root

--- index.ts
+++ index.ts
@@ -2,5 +2,6 @@ export * from "./packages/okxSmartAccount/OKXSmartContractAccount";
 export * from "./packages/okxBundler/bundler";
 export * from "./packages/okxPaymaster/paymaster";
 export * from "./packages/okxSmartAccount/types";
-export * from "./abis";
 export * from "./packages/plugins";
+export * from "./packages/common";
+export * from "./abis";
