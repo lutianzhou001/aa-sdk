a9b880eba5b02a8091f967a4db941df9ff6b9bea
vincent.lu <vincent.lu@okg.com>
2024-07-18 13:31:47 +0800

    add the test of remoteSigner

---
+++ test/remoteSigner.ts
@@ -0,0 +1,28 @@
+import {createPublicClient, http,} from "viem";
+import {arbitrum} from "viem/chains";
+import {ERC4337SmartAccount} from "../packages/erc4337SmartAccount/ERC4337SmartAccount";
+import {ExternalSigner} from "../packages/plugins/signers/externalSigner";
+import {JWT_VALIDATOR_TEMPLATE} from "../dist/packages/common/constants";
+
+async function remoteSignerTest() {
+  console.log("this is the begin of a test")
+  const client = createPublicClient({
+    chain: arbitrum,
+    transport: http(),
+  })
+
+  const owner = new ExternalSigner("signer", client as any, "0x313031323432353730323934353239343330393038", JWT_VALIDATOR_TEMPLATE)
+
+  const smartAccount = new ERC4337SmartAccount({
+    owner,
+    // accounts: (!user && !address) ? [] : _.cloneDeep(_.concat(accounts, importedAccounts)),
+    baseUrl: 'https://www.okx.com/priapi/v5/wallet/smart-account/',
+    version: "3.0.0",
+  })
+
+  const v = await smartAccount.accountManager.createNewAccount(0n, []);
+  console.log(v);
+
+}
+
+remoteSignerTest().then(r => console.log("tested"));
