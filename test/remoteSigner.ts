7f582f87947f45b6cd9b5bfdde6a957c6a034080
vincent.lu <vincent.lu@okg.com>
2024-07-18 17:27:03 +0800

    fixed test

--- test/remoteSigner.ts
+++ test/remoteSigner.ts
@@ -20,10 +20,13 @@ async function remoteSignerTest() {
     version: "3.0.0",
   })
 
-  const v = await smartAccount.accountManager.createNewAccount(0n, []);
+  await smartAccount.accountManager.createNewAccount(0n, []);
 
   const generated = await smartAccount.generateUserOperation({sigType : "EIP712", uop: {sender: "0x8e3d83375ACD5a96C7d5B53F73210651C79504a5", callData: "0x"}, _sigTime: hexToBigInt("0x69696969" as Hex)});
   console.log(generated);
+
+  const v = await smartAccount.getUOPSignedHash("EIP712", generated.userOperation);
+  console.log(v);
 }
 
 remoteSignerTest().then(r => console.log("tested"));
